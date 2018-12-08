7a4bf460f4493e6f563c149aec3d6d98476349ae
OnSwitchOff <kassov@ukr.net>
2018-12-08 17:20:16 +0200

    composer-autoload

---
+++ classes/test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: OnSwitchOff
+ * Date: 08.12.2018
+ * Time: 17:13
+ */
+namespace Acme;
+
+class test
+{
+    public $helloStr;
+
+    function __construct()
+    {
+        $this->helloStr="Hello! All";
+    }
+}
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use Acme\test;
+$obj1=new test();
+echo $obj1->helloStr;
+
+
+
+?>
