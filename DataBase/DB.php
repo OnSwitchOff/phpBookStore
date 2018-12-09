21383d80de1902f444527aaca07b4870c18b4912
OnSwitchOff <kassov@ukr.net>
2018-12-09 19:22:48 +0200

    DBColumns-class

--- DataBase/DB.php
+++ DataBase/DB.php
@@ -9,22 +9,29 @@
 
 namespace DataBase {
     use \PDO;
+    use \PDOException;
     class DB
     {
         private $user='root';
         private $pass='root';
         private $dsn='mysql:host=127.0.0.1:3306;dbname=test';
-        private $dbh;
+        private $dbh=null;
 
         function __construct()
         {
             try {
                 $this->dbh = new PDO($this->dsn, $this->user, $this->pass);
+                $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 echo "connect";
             } catch (PDOException $e) {
                 print "Error!: " . $e->getMessage() . "<br/>";
                 die();
             }
+        }
+
+        function __destruct()
+        {
+            $this->dbh=null;
         }
 
         public function createTable($table){
@@ -52,12 +59,70 @@ namespace DataBase {
                 $sth = $this->dbh->prepare($table->selectAllSql());
                 var_dump($sth->execute());
                 $result = $sth->fetchAll();
-                print_r($result);
+                return $result;
             }catch (PDOException $e) {
                 print "Error!: " . $e->getMessage() . "<br/>";
                 die();
             }
         }
+
+        public function getAll($sql, $params = [])
+        {
+            $sth = $this->dbh->prepare($sql);
+            $data = [];
+            if ($sth->execute($params)) {
+                while (($row = $sth->fetch(PDO::FETCH_ASSOC))) {
+                    $data[] = $row;
+                }
+            }
+            return $data;
+        }
+
+        public function getObjectsOf($query, $class)
+        {
+            $tmpResult = $this->dbh>query($query);
+            $tmpResult->setFetchMode(PDO::FETCH_CLASS, $class);
+            $result = [];
+
+            while ($object = $tmpResult->fetch()) {
+                $result[] = $object;
+            }
+
+            return $result;
+        }
+
+        public function query($query)
+        {
+            $result = [];
+            $sth = $this->dbh->prepare($query);
+            $sth->execute();
+            $sth->setFetchMode(PDO::FETCH_ASSOC);
+
+            while ($row = $sth->fetch()) {
+                $result[] = $row;
+            }
+
+            return $result;
+        }
+
+        public function getOne($query)
+        {
+            $sth = $this->dbh->prepare($query);
+            $sth->execute();
+            $result = $sth->fetch(PDO::FETCH_COLUMN);
+            return $result;
+        }
+
+        public function getOneObject($query, $class)
+        {
+            $sth= $this->dbh->prepare($query);
+            $sth->execute();
+            $sth->setFetchMode(PDO::FETCH_CLASS, $class);
+            $result = $sth->fetch();
+            return $result;
+        }
+
+
 
         public function insertRow($table,$values){
             try{
